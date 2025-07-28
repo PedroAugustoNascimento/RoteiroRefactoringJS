fce847f9d7476c37f5f14b229648f88a196ba9f6
Pedro Augusto Nascimento <nascimento.pedroaug@gmail.com>
2025-07-28 19:26:15 -0300

    Commit 5 - Move Function

--- index.js
+++ index.js
@@ -1,76 +1,5 @@
 const { readFileSync } = require('fs');
 
-function gerarFaturaStr(fatura, pecas) {
-  let totalFatura = 0;
-  let creditos = 0;
-
-  function getPeca(apresentacao) {
-    return pecas[apresentacao.id];
-  }
-
-  // const peca = pecas[apre.id]; -> Segundo refactoring
-  function calcularTotalApresentacao(apre) {
-    let total = 0;
-    switch (getPeca(apre).tipo) {
-      case "tragedia":
-        total = 40000;
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30);
-        }
-        break;
-      case "comedia":
-        total = 30000;
-        if (apre.audiencia > 20) {
-          total += 10000 + 500 * (apre.audiencia - 20);
-        }
-        total += 300 * apre.audiencia;
-        break;
-      default:
-        throw new Error(`Peca desconhecia: ${getPeca(apre).tipo}`);
-    }
-    return total;
-  }
-
-  // créditos para próximas contratações (Commit 2)
-  function calcularCredito(apre) {
-    let creditos = 0;
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (getPeca(apre).tipo === "comedia")
-      creditos += Math.floor(apre.audiencia / 5);
-    return creditos;
-  }
-
-  function calcularTotalCredito() { // -> Commit 4 - Função calcular total credito
-    let creditos = 0;
-    for (let apre of fatura.apresentacoes) {
-      creditos += calcularCredito(apre);
-    }
-    return creditos;
-  }
-
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
-  for (let apre of fatura.apresentacoes) {
-    const peca = getPeca(apre);
-    let total = calcularTotalApresentacao(apre);
-
-    // Função FormatarMoeda incluída (Commit 3)
-    faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
-    totalFatura += total;
-
-  function calcularTotalFatura(faturaJSON) {
-    let fatura = 0;
-    for (let apre of faturaJSON.apresentacoes) {
-      fatura += calcularTotalApresentacao(apre); // ERRO ESTAVA AQUI -> função estava fora do escopo
-  }
-    return fatura;
-}
-  }
-
-  faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura(fatura))}\n`; // -> Função formatarMoeda incluída (Commit 3)
-  faturaStr += `Créditos acumulados: ${calcularTotalCredito()} \n`;
-  return faturaStr;
-}
-
 // função para formatar a moeda (Commit 3)
 function formatarMoeda(valor) {
   return new Intl.NumberFormat("pt-BR",
@@ -80,14 +9,77 @@ function formatarMoeda(valor) {
     }).format(valor / 100);
 }
 
-// OBS: Esta versão da função está sobrescrita — mantida por clareza
-function calcularTotalFatura() {
-  let totalFatura;
-  let total;
-  return totalFatura += total / 100;
+// função de get para as peças (Commit 2)
+function getPeca(apresentacao, pecas) {
+  return pecas[apresentacao.id];
 }
 
+// créditos para próximas contratações (Commit 2)
+function calcularCredito(apre, pecas) {
+  let creditos = 0;
+  creditos += Math.max(apre.audiencia - 30, 0);
+  if (getPeca(apre, pecas).tipo === "comedia")
+    creditos += Math.floor(apre.audiencia / 5);
+  return creditos;
+}
 
+function calcularTotalCredito(pecas, apresentacoes) { // -> Commit 4 - Função calcular total credito
+  let creditos = 0;
+  for (let apre of apresentacoes) {
+    creditos += calcularCredito(apre, pecas); 
+  }
+  return creditos;
+}
+
+// função para calcular o total da apresentação (Commit 4)
+function calcularTotalApresentacao(apre, pecas) {
+  let total = 0;
+  switch (getPeca(apre, pecas).tipo) { 
+    case "tragedia":
+      total = 40000;
+      if (apre.audiencia > 30) {
+        total += 1000 * (apre.audiencia - 30);
+      }
+      break;
+    case "comedia":
+      total = 30000;
+      if (apre.audiencia > 20) {
+        total += 10000 + 500 * (apre.audiencia - 20);
+      }
+      total += 300 * apre.audiencia;
+      break;
+    default:
+      throw new Error(`Peca desconhecia: ${getPeca(apre, pecas).tipo}`); 
+  }
+  return total;
+}
+
+// função para calcular o total da fatura (Commit 5)
+function calcularTotalFatura(faturaJSON, pecas) {
+  let fatura = 0;
+  for (let apre of faturaJSON.apresentacoes) {
+    fatura += calcularTotalApresentacao(apre, pecas); 
+  }
+  return fatura;
+}
+
+// realocando a posição e corrigindo alguns códigos (Commit 5)
+function gerarFaturaStr(fatura, pecas) {
+  let totalFatura = 0;
+  let faturaStr = `Fatura ${fatura.cliente}\n`;
+  for (let apre of fatura.apresentacoes) {
+    const peca = getPeca(apre, pecas); // 
+    let total = calcularTotalApresentacao(apre, pecas); 
+
+    // Função FormatarMoeda incluída (Commit 3)
+    faturaStr += `  ${getPeca(apre, pecas).nome}: ${formatarMoeda(calcularTotalApresentacao(apre, pecas))} (${apre.audiencia} assentos)\n`; 
+    totalFatura += total;
+  }
+
+  faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura(fatura, pecas))}\n`; // -> Função formatarMoeda incluída (Commit 3) 
+  faturaStr += `Créditos acumulados: ${calcularTotalCredito(pecas, fatura.apresentacoes)} \n`; 
+  return faturaStr;
+}
 
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
